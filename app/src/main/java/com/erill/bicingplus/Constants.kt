b60b7348cbf8ecb9b85c8ddc3f12df5c9a331986
Roger Erill <rogerguddy@gmail.com>
2017-06-19 23:24:07 +0200

    Load user's location

--- app/src/main/java/com/erill/bicingplus/Constants.kt
+++ app/src/main/java/com/erill/bicingplus/Constants.kt
@@ -8,3 +8,6 @@ val BASE_URL = "http://wservice.viabicing.cat/v2/"
 val DEFAULT_LAT = 41.4
 val DEFAULT_LON = 2.18
 val DEFAULT_ZOOM = 14f
+val CLOSE_ZOOM = 16f
+val UPDATE_INTERVAL = 30000L
+val FASTEST_UPDATE_INTERVAL = 5000L
--- app/src/main/java/com/erill/bicingplus/main/MainActivity.kt
+++ app/src/main/java/com/erill/bicingplus/main/MainActivity.kt
@@ -2,14 +2,21 @@ package com.erill.bicingplus.main
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.erill.bicingplus.*
 import com.erill.bicingplus.main.di.MainModule
 import com.erill.bicingplus.ws.responses.BicingResponse
 import com.erill.bicingplus.ws.responses.BikeType
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderApi
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
@@ -21,24 +28,120 @@ import javax.inject.Inject
 
 
 
-class MainActivity : AppCompatActivity(), MainView {
+class MainActivity : AppCompatActivity(), MainView,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks {
 
     @Inject lateinit var presenter: MainPresenter
 
     val component by lazy { app.component.plus(MainModule(this)) }
     var googleMap: GoogleMap? = null
-    private val LOCATION_REQUEST_CODE: Int = 1
+    val LOCATION_REQUEST_CODE: Int = 1
+    private val TAG = "MapActivity"
+
+    var googleApiClient: GoogleApiClient? = null
+    var fusedLocationClient: FusedLocationProviderApi? = null
+    var lastLocation: Location? = null
+    var locationRequest: LocationRequest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         component.inject(this)
 
+        buildGoogleApiClient()
+        createLocationRequest()
+        fusedLocationClient = LocationServices.FusedLocationApi
+
         presenter.loadStations()
 
         val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync {
-            loadedMap -> loadDefaultLocation(loadedMap)
+            loadedMap -> loadLastLocation(loadedMap)
+        }
+    }
+
+    @Synchronized private fun buildGoogleApiClient() {
+        googleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+
+        googleApiClient?.connect()
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest?.interval = UPDATE_INTERVAL
+        locationRequest?.fastestInterval = FASTEST_UPDATE_INTERVAL
+        locationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (googleApiClient?.isConnected as Boolean) {
+            startLocationUpdates()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient?.removeLocationUpdates(googleApiClient) { Log.d(TAG, "Stop updates")}
+    }
+
+    private fun startLocationUpdates() {
+        fusedLocationClient?.requestLocationUpdates(googleApiClient, locationRequest) {
+            location ->
+            Log.d(TAG, "New location ${location.latitude} - ${location.longitude}")
+            lastLocation = location
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), CLOSE_ZOOM)
+            googleMap?.animateCamera(cameraUpdate)
+        }
+    }
+
+    private fun loadLastLocation(loadedMap: GoogleMap?) {
+
+        googleMap = loadedMap
+        lastLocation = fusedLocationClient?.getLastLocation(googleApiClient)
+
+        val latLong: LatLng
+        val zoomLevel: Float
+        if (lastLocation != null) {
+            latLong = LatLng(lastLocation!!.latitude, lastLocation!!.longitude)
+            zoomLevel = CLOSE_ZOOM
+        }
+        else {
+            latLong = LatLng(DEFAULT_LAT, DEFAULT_LON)
+            zoomLevel = DEFAULT_ZOOM
+        }
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLong, zoomLevel))
+        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json))
+        googleMap?.uiSettings?.isZoomControlsEnabled = true
+        checkLocation()
+    }
+
+    private fun checkLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            googleMap?.isMyLocationEnabled = true
+            /*googleMap?.setOnMyLocationButtonClickListener({
+                loadLastLocation(googleMap, CLOSE_ZOOM)
+                true
+            })*/
+        } else {
+            val permissionArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissionArray, LOCATION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            googleMap?.isMyLocationEnabled = permissions.size == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
         }
     }
 
@@ -55,35 +158,6 @@ class MainActivity : AppCompatActivity(), MainView {
         }
     }
 
-    private fun loadDefaultLocation(loadedMap: GoogleMap?) {
-        googleMap = loadedMap
-        val bcn = LatLng(DEFAULT_LAT, DEFAULT_LON)
-        /*loadedMap?.addMarker(MarkerOptions().position(bcn)
-                .title("My marker"))*/
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(bcn, DEFAULT_ZOOM))
-        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json))
-        googleMap?.uiSettings?.isZoomControlsEnabled = true
-        checkLocation()
-    }
-
-    private fun checkLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            googleMap?.isMyLocationEnabled = true
-        } else {
-            val permissionArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-            ActivityCompat.requestPermissions(this, permissionArray, LOCATION_REQUEST_CODE)
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == LOCATION_REQUEST_CODE) {
-            googleMap?.isMyLocationEnabled = permissions.size == 1 &&
-                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED
-        }
-    }
-
     override fun showProgress() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -91,4 +165,19 @@ class MainActivity : AppCompatActivity(), MainView {
     override fun hideProgress() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    override fun onConnected(bundle: Bundle?) {
+        Log.d(TAG, "Connected")
+        startLocationUpdates()
+    }
+
+    override fun onConnectionSuspended(int: Int) {
+        Log.d(TAG, "Connection Suspended")
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.d(TAG, "Connection Failed: ${connectionResult.errorMessage}")
+    }
+
+
 }
